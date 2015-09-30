601454f647ddd741566ee531483b5fa310db7db1
Addison Correll <acorrell5106@stu.neisd.net>
2015-09-30 07:45:11 -0500

    Beginning (1st NetBeans Program)

---
+++ src/correll_4_datatypes/Correll_4_datatypes.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correll_4_datatypes;
+
+/**
+ *
+ * @author acorrell5106
+ */
+public class Correll_4_datatypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*space for comments, used to explain code
+        code explanation is an expectation in d22*/
+        int i = 0;
+        //f is for float, instead of a double, good for decimals
+        float a = 0f;
+        double b = 11.0;
+        short c = 100;
+        //char needs ''
+        char d1 = '@';
+        char d2 = 'b';
+        char d3 = '3';
+        //String needs ""
+        String e = "ninja";
+        //boolean is either true or false
+        boolean f = false;
+        boolean g = i % 2 == 0;
+        /*
+        here we will look at some mathematical operators
+        
+        + this adds two numbers or more
+        - this subtracts two numbers or more
+        * this multiples two numbers or more
+        / this divides two numbers or more
+        % this is modulo. Divides and shows the remainder
+        */
+        
+        /*System.out.println(i + " is an integer");
+        System.out.println(a + " is a float");
+        System.out.println(b + " is a double");
+        System.out.println(c + " is a short");
+        System.out.println(d1 + " is a char");
+        System.out.println(d2 + " is a char");
+        System.out.println(d3 + " is a char");
+        System.out.println(e + " is a string");
+        System.out.println(f + " is a boolean");
+        System.out.println(g + " is a boolean");*/
+        
+        /*i = 0;
+        a = 0;
+        e = "ninja";
+        System.out.println(i + a + " an integer" + " plus a float ");
+        //upcast to float is automatic, i.e. 7 + 0.0 = 7.0
+        for(i = -123; i < 123; i++){
+        //== says is this equal? True or false?
+        System.out.println(i % 4 == 0);*/
+        
+        /*i = 22;
+        a = 0;
+        e = "ninja";
+        System.out.println(e + i + " an" + " a string plus an integer");*/
+        /*adding an int and a string concatenates (combines) the int and string
+         as a string*/
+        //a = 2.5f;
+        //b = 3.2;
+        //System.out.println(a + b "a float plus an double");
+        //b = 3.1;
+        //c = 16;
+        //System.out.println(b + c +" is a double" + " plus a short");
+        //for (i = 0; i < 500; i++) {
+            /*if is a conditional (flow) statement. If a condition is true the following command will be executed*/
+        
+
+        //if (i % 2 == 0) {
+            //System.out.println(i + " is even");
+        //} else {
+            //System.out.println(i + " is odd");
+        for (i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is not a prime");
+            }
+            if (i % 3 == 0) {
+                System.out.println(i + " is not a prime");
+
+                
+                }
+          
+                
+        }
+    }
+    }
+        //% checks for the remainder
+        //i % 2 checks if a number is even or odd
+        
+        
+    
+   
+    
